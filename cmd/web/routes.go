1b159cfc88b3160ccfb9c23297e859d07de421bd
porky256 <61063240+porky256@users.noreply.github.com>
2023-05-13 11:28:57 +0200

    add recoverer

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,14 +1,17 @@
 package main
 
 import (
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/porky256/course-project/pkg/config"
 	"github.com/porky256/course-project/pkg/handlers"
 	"net/http"
 )
 
 func routes(app *config.AppConfig, handler *handlers.Handlers) http.Handler {
-	mux := pat.New()
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
 
 	mux.Get("/", http.HandlerFunc(handler.Home))
 	mux.Get("/about", http.HandlerFunc(handler.About))

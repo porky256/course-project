f46ee0ac6c27279292d29de36c5cadfed8558ba1
porky256 <61063240+porky256@users.noreply.github.com>
2023-05-17 20:21:24 +0200

    fix handlers POST testcases

--- internal/handlers/handlers_test.go
+++ internal/handlers/handlers_test.go
@@ -14,7 +14,13 @@ import (
 	"github.com/porky256/course-project/internal/render"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 )
+
+type params struct {
+	key   string
+	value string
+}
 
 var _ = Describe("Handlers", Ordered, func() {
 
@@ -32,14 +38,26 @@ var _ = Describe("Handlers", Ordered, func() {
 	}
 
 	var postTestCases = []struct {
-		url    string
-		params []struct {
-			key   string
-			value string
-		}
+		url                string
+		params             []params
 		expectedStatusCode int
 	}{
-		{url: "/", params},
+		{url: "/make-reservation", params: []params{
+			{key: "start", value: "2023-01-02"},
+			{key: "end", value: "2023-01-04"},
+		}, expectedStatusCode: http.StatusOK},
+		{url: "/search-availability", params: []params{
+			{key: "first_name", value: "John"},
+			{key: "last_name", value: "Black"},
+			{key: "email", value: "test@test.com"},
+			{key: "phone", value: "555-555-5555"},
+		}, expectedStatusCode: http.StatusOK},
+		{url: "/search-availability-json", params: []params{
+			{key: "first_name", value: "John"},
+			{key: "last_name", value: "Black"},
+			{key: "email", value: "test@test.com"},
+			{key: "phone", value: "555-555-5555"},
+		}, expectedStatusCode: http.StatusOK},
 	}
 
 	var server *httptest.Server
@@ -57,6 +75,19 @@ var _ = Describe("Handlers", Ordered, func() {
 		for _, testCase := range getTestCases {
 			By(fmt.Sprintf("testing '%s' page", testCase.url))
 			resp, err := server.Client().Get(server.URL + testCase.url)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(resp.StatusCode).To(Equal(testCase.expectedStatusCode))
+		}
+	})
+
+	It("check POST handlers", func() {
+		for _, testCase := range postTestCases {
+			By(fmt.Sprintf("testing '%s' page", testCase.url))
+			values := url.Values{}
+			for _, value := range testCase.params {
+				values.Add(value.key, value.value)
+			}
+			resp, err := server.Client().PostForm(server.URL+testCase.url, values)
 			Expect(err).ToNot(HaveOccurred())
 			Expect(resp.StatusCode).To(Equal(testCase.expectedStatusCode))
 		}
